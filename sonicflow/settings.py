1afbc715f7db8c29e5105d9f6c2a287a33735b4e
Rahul Chatterjee <rc5292684@gmail.com>
2025-05-13 11:54:00 +0530

    cors error

--- sonicflow/settings.py
+++ sonicflow/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
- 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,7 +59,12 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'sonicflow.urls'
 
 
-ALLOWED_HOSTS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://sonicflow-backend-2.onrender.com',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 REST_FRAMEWORK = {
